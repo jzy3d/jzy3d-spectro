ad01cff2e37e1a1dca2960ac02cc464cebcc12fb
Martin Pernollet <mpernollet@octo.com>
2020-11-13 21:09:35 +0100

    Cleanup GL, GLU, Camera from Drawables

--- src/api/org/jzy3d/spectro/primitives/SpectrumSurface.java
+++ src/api/org/jzy3d/spectro/primitives/SpectrumSurface.java
@@ -6,11 +6,9 @@ import org.jzy3d.colors.ColorMapper;
 import org.jzy3d.colors.colormaps.ColorMapRainbow;
 import org.jzy3d.maths.BoundingBox3d;
 import org.jzy3d.maths.Range;
-import org.jzy3d.painters.GLES2CompatUtils;
 import org.jzy3d.painters.Painter;
 import org.jzy3d.plot3d.primitives.Geometry;
 
-import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
 
 /**
@@ -21,154 +19,152 @@ import com.jogamp.opengl.GL2;
  * @author Martin Pernollet
  */
 public class SpectrumSurface extends Geometry {
-    /**
-     * The total frequency range covered by a frame always matches the frequency
-     * range of the input samples, which according to the Nyquist theorem is 1/2
-     * the sampling rate (so for a 44100hz wav file, that's 0-22050hz).
-     */
-    public static double maxFreqOfSpectrum = 22050;
+	/**
+	 * The total frequency range covered by a frame always matches the frequency
+	 * range of the input samples, which according to the Nyquist theorem is 1/2 the
+	 * sampling rate (so for a 44100hz wav file, that's 0-22050hz).
+	 */
+	public static double maxFreqOfSpectrum = 22050;
 
-    /**
-     * actual range of interest in frame spectrum. Spectrum might have 1024
-     * dimension, but only 50 interesting in .
-     */
-    protected int maxFreqId;
-    protected double maxFreqValue;
+	/**
+	 * actual range of interest in frame spectrum. Spectrum might have 1024
+	 * dimension, but only 50 interesting in .
+	 */
+	protected int maxFreqId;
+	protected double maxFreqValue;
 
-    protected int frameWidth;
-    protected int frameCount;
-    protected boolean absoluteZ = true;
+	protected int frameWidth;
+	protected int frameCount;
+	protected boolean absoluteZ = true;
 
-    protected SpectrumModel spectrum;
-    protected Range rangeX;
-    protected Range rangeY;
-    protected Range rangeZ;
+	protected SpectrumModel spectrum;
+	protected Range rangeX;
+	protected Range rangeY;
+	protected Range rangeZ;
 
-    public SpectrumSurface(SpectrumModel spectrum) {
-        this(spectrum, Integer.MAX_VALUE);
-    }
+	public SpectrumSurface(SpectrumModel spectrum) {
+		this(spectrum, Integer.MAX_VALUE);
+	}
 
-    public SpectrumSurface(SpectrumModel spectrum, int maxFreqId) {
-        // spectrum data
-        this.spectrum = spectrum;
-        this.maxFreqId = maxFreqId;
-        this.frameCount = spectrum.getFrameCount();
-        this.frameWidth = Math.min(spectrum.getFrameWidth(), maxFreqId);
-        this.maxFreqValue = idToFreq(frameWidth, frameWidth);
+	public SpectrumSurface(SpectrumModel spectrum, int maxFreqId) {
+		// spectrum data
+		this.spectrum = spectrum;
+		this.maxFreqId = maxFreqId;
+		this.frameCount = spectrum.getFrameCount();
+		this.frameWidth = Math.min(spectrum.getFrameWidth(), maxFreqId);
+		this.maxFreqValue = idToFreq(frameWidth, frameWidth);
 
-        // bounding box
-        rangeX = new Range(0, frameCount);
-        rangeY = new Range(0, (float)maxFreqValue);
-        rangeZ = processSpectralValuesRange(spectrum);
-        if (absoluteZ)
-            rangeZ.setMin(0);
-        bbox = new BoundingBox3d(rangeX, rangeY, rangeZ);
-        
-        // coloring policy
-        mapper = new ColorMapper(new ColorMapRainbow(), rangeZ);
-        
-        Logger.getLogger(SpectrumSurface.class).info(frameCount + " frames of " + frameWidth + " dims assuming max frequency " + maxFreqOfSpectrum);
-    }
+		// bounding box
+		rangeX = new Range(0, frameCount);
+		rangeY = new Range(0, (float) maxFreqValue);
+		rangeZ = processSpectralValuesRange(spectrum);
+		if (absoluteZ)
+			rangeZ.setMin(0);
+		bbox = new BoundingBox3d(rangeX, rangeY, rangeZ);
 
-    @Override
-    protected void begin(Painter painter, GL gl) {
-        if (gl.isGL2()) {
-            gl.getGL2().glBegin(geometry());
-        } else {
-            GLES2CompatUtils.glBegin(geometry());
-        }
-    }
+		// coloring policy
+		mapper = new ColorMapper(new ColorMapRainbow(), rangeZ);
 
-    protected int geometry() {
-        return GL2.GL_POLYGON;
-    }
+		Logger.getLogger(SpectrumSurface.class)
+				.info(frameCount + " frames of " + frameWidth + " dims assuming max frequency " + maxFreqOfSpectrum);
+	}
 
-    /** drawing polygons from spectrum values directly */
-    @Override
-    public void callPointsForFace(Painter painter, GL gl) {
-        for (int framId = 0; framId < frameCount - 1; framId++) {
-            int frameMax = Math.min(spectrum.getFrameWidth() - 1, frameWidth);
-            for (int freqId = 0; freqId < frameMax; freqId++) {
-                drawPolygon(painter, gl, spectrum, framId, freqId);
-            }
-        }
-    }
+	@Override
+	protected void begin(Painter painter) {
+		painter.glBegin(geometry());
+	}
 
-    protected void drawPolygon(Painter painter, GL gl, SpectrumModel spectrum, int frameId, int freqId) {
-        float x0 = frameId;
-        float x1 = frameId + 1;
-        float y0 = adaptY(freqId);
-        float y1 = adaptY(freqId + 1);
-        float zX0Y0 = adaptZ(spectrum.getEnergy(frameId, freqId));
-        float zX1Y0 = adaptZ(spectrum.getEnergy(frameId+1, freqId));
-        float zX1Y1 = adaptZ(spectrum.getEnergy(frameId+1, freqId + 1));
-        float zX0Y1 = adaptZ(spectrum.getEnergy(frameId, freqId + 1));
+	protected int geometry() {
+		return GL2.GL_POLYGON;
+	}
 
-        callPolygonPoints(painter, gl, x0, x1, y0, y1, zX0Y0, zX1Y0, zX1Y1, zX0Y1);
-    }
+	/** drawing polygons from spectrum values directly */
+	@Override
+	public void callPointsForFace(Painter painter) {
+		for (int framId = 0; framId < frameCount - 1; framId++) {
+			int frameMax = Math.min(spectrum.getFrameWidth() - 1, frameWidth);
+			for (int freqId = 0; freqId < frameMax; freqId++) {
+				drawPolygon(painter, spectrum, framId, freqId);
+			}
+		}
+	}
 
-    protected void callPolygonPoints(Painter painter, GL gl, float x0, float x1, float y0, float y1, float zX0Y0, float zX1Y0, float zX1Y1, float zX0Y1) {
-        begin(painter, gl);
-        if (mapper != null) {
-        	painter.color(mapper.getColor(zX0Y0));
-        } else {
-        	painter.color(Color.BLACK);
-        }
+	protected void drawPolygon(Painter painter, SpectrumModel spectrum, int frameId, int freqId) {
+		float x0 = frameId;
+		float x1 = frameId + 1;
+		float y0 = adaptY(freqId);
+		float y1 = adaptY(freqId + 1);
+		float zX0Y0 = adaptZ(spectrum.getEnergy(frameId, freqId));
+		float zX1Y0 = adaptZ(spectrum.getEnergy(frameId + 1, freqId));
+		float zX1Y1 = adaptZ(spectrum.getEnergy(frameId + 1, freqId + 1));
+		float zX0Y1 = adaptZ(spectrum.getEnergy(frameId, freqId + 1));
 
-        painter.glVertex3f(x0, y0, zX0Y0);
-        painter.glVertex3f(x1, y0, zX1Y0);
-        painter.glVertex3f(x1, y1, zX1Y1);
-        painter.glVertex3f(x0, y1, zX0Y1);
-        painter.glEnd();
-    }
+		callPolygonPoints(painter, x0, x1, y0, y1, zX0Y0, zX1Y0, zX1Y1, zX0Y1);
+	}
 
-    protected float adaptY(int value) {
-        return (float)idToFreq(value, frameWidth);
-    }
+	protected void callPolygonPoints(Painter painter, float x0, float x1, float y0, float y1, float zX0Y0, float zX1Y0,
+			float zX1Y1, float zX0Y1) {
+		begin(painter);
+		if (mapper != null) {
+			painter.color(mapper.getColor(zX0Y0));
+		} else {
+			painter.color(Color.BLACK);
+		}
 
-    protected float adaptZ(double value) {
-        if (absoluteZ)
-            return (float)Math.abs(value);
-        else
-            return (float)value;
-    }
+		painter.glVertex3f(x0, y0, zX0Y0);
+		painter.glVertex3f(x1, y0, zX1Y0);
+		painter.glVertex3f(x1, y1, zX1Y1);
+		painter.glVertex3f(x0, y1, zX0Y1);
+		painter.glEnd();
+	}
 
-    /** no wireframe */
-    @Override
-    public void callPointForWireframe(Painter painter, GL gl) {
-    }
+	protected float adaptY(int value) {
+		return (float) idToFreq(value, frameWidth);
+	}
 
-    /* UTILS */
+	protected float adaptZ(double value) {
+		if (absoluteZ)
+			return (float) Math.abs(value);
+		else
+			return (float) value;
+	}
 
-    public double idToFreq(int i, int n) {
-        double r = i / (double) n;
-        return r * maxFreqOfSpectrum;
-        // log2(r*maxFreq);
-    }
+	/** no wireframe */
+	@Override
+	public void callPointForWireframe(Painter painter) {
+	}
 
-    public double log2(double x) {
-        return Math.log(x) / Math.log(2);
-    }
+	/* UTILS */
 
-    protected Range processSpectralValuesRange(SpectrumModel spectrum) {
-        float min = Float.MAX_VALUE;
-        float max = Float.MIN_VALUE;
+	public double idToFreq(int i, int n) {
+		double r = i / (double) n;
+		return r * maxFreqOfSpectrum;
+		// log2(r*maxFreq);
+	}
 
-        for (int framId = 0; framId < frameCount; framId++) {
-            //Frame frame = clip.getFrame(framId);
+	public double log2(double x) {
+		return Math.log(x) / Math.log(2);
+	}
 
-            for (int freqId = 0; freqId < frameWidth; freqId++) {
-                float value = (float)spectrum.getEnergy(framId, freqId);
+	protected Range processSpectralValuesRange(SpectrumModel spectrum) {
+		float min = Float.MAX_VALUE;
+		float max = Float.MIN_VALUE;
 
-                if (min > value) {
-                    min = value;
-                }
-                if (max < value) {
-                    max = value;
-                }
-            }
-        }
-        Range range = new Range(min, max);
-        return range;
-    }
+		for (int framId = 0; framId < frameCount; framId++) {
+			// Frame frame = clip.getFrame(framId);
+
+			for (int freqId = 0; freqId < frameWidth; freqId++) {
+				float value = (float) spectrum.getEnergy(framId, freqId);
+
+				if (min > value) {
+					min = value;
+				}
+				if (max < value) {
+					max = value;
+				}
+			}
+		}
+		Range range = new Range(min, max);
+		return range;
+	}
 }
